31d00589ed2e946929656aebca956f22b6c2530b
Ryddag <projectceleste0@gmail.com>
2017-12-20 18:19:57 +0100

    ...

--- Libs/Celeste_Public_Api/GameScanner_Api/Models/GameFilesInfo.cs
+++ Libs/Celeste_Public_Api/GameScanner_Api/Models/GameFilesInfo.cs
@@ -1,7 +1,10 @@
 ﻿#region Using directives
 
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Xml.Serialization;
+using Celeste_Public_Api.Helpers;
 
 #endregion
 
@@ -31,5 +34,29 @@ namespace Celeste_Public_Api.GameScanner_Api.Models
 
         [XmlAttribute(AttributeName = "BinSize")]
         public long BinSize { get; set; }
+    }
+
+    [XmlRoot(ElementName = "FilesInfo")]
+    public class GameFilesInfo
+    {
+        [XmlIgnore]
+        public Dictionary<string, GameFileInfo> FileInfo { get; } = new Dictionary<string, GameFileInfo>();
+
+        [XmlElement(ElementName = "FilesInfo")]
+        public GameFileInfo[] FilesInfoArray
+        {
+            get => FileInfo.Values.ToArray();
+            set
+            {
+                if (value == null) return;
+                foreach (var item in value)
+                    FileInfo.Add(item.FileName.ToLower(), item);
+            }
+        }
+
+        public void ToXml(string filename)
+        {
+            XmlUtils.SerializeToFile(this, filename);
+        }
     }
 }
