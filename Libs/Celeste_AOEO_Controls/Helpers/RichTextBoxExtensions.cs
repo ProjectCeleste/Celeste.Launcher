4c655ff3b18c04fc789b0664d94ac09bbd4a0698
Ryddag <projectceleste0@gmail.com>
2019-05-20 16:09:43 +0200

    Nuget packages update

---
+++ Libs/Celeste_AOEO_Controls/Helpers/RichTextBoxExtensions.cs
@@ -0,0 +1,71 @@
+﻿#region Using directives
+
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+#endregion
+
+namespace Celeste_Launcher_Gui.Helpers
+{
+    //https://stackoverflow.com/questions/2914004/rich-text-box-padding-between-text-and-border/33542937
+    public static class RichTextBoxExtensions
+    {
+        private const int EmGetrect = 0xB2;
+        private const int EmSetrect = 0xB3;
+
+        public static void SetInnerMargins(this TextBoxBase textBox, int left, int top, int right, int bottom)
+        {
+            var rect = textBox.GetFormattingRect();
+
+            var newRect = new Rectangle(left, top, rect.Width - left - right, rect.Height - top - bottom);
+            textBox.SetFormattingRect(newRect);
+        }
+
+        [DllImport(@"User32.dll", EntryPoint = @"SendMessage", CharSet = CharSet.Auto)]
+        private static extern int SendMessageRefRect(IntPtr hWnd, uint msg, int wParam, ref RECT rect);
+
+        [DllImport(@"user32.dll", EntryPoint = @"SendMessage", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, ref Rectangle lParam);
+
+        private static void SetFormattingRect(this IWin32Window textbox, Rectangle rect)
+        {
+            var rc = new RECT(rect);
+            SendMessageRefRect(textbox.Handle, EmSetrect, 0, ref rc);
+        }
+
+        private static Rectangle GetFormattingRect(this IWin32Window textbox)
+        {
+            var rect = new Rectangle();
+            SendMessage(textbox.Handle, EmGetrect, (IntPtr) 0, ref rect);
+            return rect;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        // ReSharper disable once InconsistentNaming
+        private struct RECT
+        {
+            // ReSharper disable MemberCanBePrivate.Local
+            public readonly int Left;
+
+            public readonly int Top;
+            public readonly int Right;
+
+            public readonly int Bottom;
+            // ReSharper restore MemberCanBePrivate.Local
+
+            private RECT(int left, int top, int right, int bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom = bottom;
+            }
+
+            public RECT(Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom)
+            {
+            }
+        }
+    }
+}
