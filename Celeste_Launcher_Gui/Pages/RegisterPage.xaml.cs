5e548f75426f1518a3368b84500b7d21dfd676d4
unknown <martin_mine@hotmail.com>
2019-08-09 16:38:35 +0200

    Integrate registration

--- Celeste_Launcher_Gui/Pages/RegisterPage.xaml.cs
+++ Celeste_Launcher_Gui/Pages/RegisterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Celeste_Launcher_Gui.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,72 @@ namespace Celeste_Launcher_Gui.Pages
         public RegisterPage()
         {
             InitializeComponent();
+        }
+
+        private void OnResendVerificationKey(object sender, RoutedEventArgs args)
+        {
+            OnVerifyEmail(sender, args);
+        }
+
+        private async void OnVerifyEmail(object sender, RoutedEventArgs args)
+        {
+            VerifyEmailBtn.IsEnabled = false;
+            ResentKeyBtn.IsEnabled = false;
+
+            try
+            {
+                var response = await LegacyBootstrapper.WebSocketApi.DoValidMail(EmailField.LabelContent);
+
+                if (response.Result)
+                {
+                    GenericMessageDialog.Show($@"{response.Message}", DialogIcon.Warning);
+                }
+                else
+                {
+                    GenericMessageDialog.Show($@"Error: {response.Message}", DialogIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                GenericMessageDialog.Show($"Error: {ex.Message}", DialogIcon.Error);
+            }
+
+            VerifyEmailBtn.IsEnabled = true;
+            ResentKeyBtn.IsEnabled = true;
+        }
+
+        private async void OnRegister(object sender, RoutedEventArgs args)
+        {
+            if (ConfirmPasswordField.PasswordInputBox.Password != PasswordField.PasswordInputBox.Password)
+            {
+                GenericMessageDialog.Show(@"The confirmed password must match the first password", DialogIcon.Error);
+                return;
+            }
+
+            RegisterBtn.IsEnabled = false;
+
+            try
+            {
+                var response = await LegacyBootstrapper.WebSocketApi.DoRegister(EmailField.LabelContent, VerifyKeyField.LabelContent, 
+                    UsernameField.LabelContent, PasswordField.PasswordInputBox.Password);
+
+                if (response.Result)
+                {
+                    GenericMessageDialog.Show($@"{response.Message}", DialogIcon.Error);
+
+                    NavigationService.Navigate(new Uri("Pages/MainMenuPage.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    GenericMessageDialog.Show($@"Error: {response.Message}", DialogIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                GenericMessageDialog.Show($"Error: {ex.Message}", DialogIcon.Error);
+            }
+
+            RegisterBtn.IsEnabled = true;
         }
     }
 }
