7de887ab5ffc09c97563cb9f9a436dae4ad174f6
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2019-08-18 20:36:53 +0200

    New icon and rename launcher

--- Celeste_Launcher_Gui/Properties/AssemblyInfo.cs
+++ Celeste_Launcher_Gui/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Celeste Launcher")]
+[assembly: AssemblyTitle("Project Celeste Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Celeste Launcher")]
+[assembly: AssemblyProduct("Project Celeste Launcher")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
