bacd9f13cae2dcb23dbaa0b1d25a6eabfe89d31e
Ryddag <projectceleste0@gmail.com>
2019-08-19 15:01:22 +0000

    v2.0.8.2

--- Celeste_Launcher_Gui/Properties/AssemblyInfo.cs
+++ Celeste_Launcher_Gui/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("2.0.8.1")]
-[assembly: AssemblyVersion("2.0.8.1")]
-[assembly: AssemblyFileVersion("2.0.8.1")]
+// [assembly: AssemblyVersion("2.0.8.2")]
+[assembly: AssemblyVersion("2.0.8.2")]
+[assembly: AssemblyFileVersion("2.0.8.2")]
