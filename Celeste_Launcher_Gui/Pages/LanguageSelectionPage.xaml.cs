59f2f4217b183878bcf72e418fb54a763295f5a0
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2019-08-21 18:12:38 +0200

    Hook up language selection menu, add br-pt infra

--- Celeste_Launcher_Gui/Pages/LanguageSelectionPage.xaml.cs
+++ Celeste_Launcher_Gui/Pages/LanguageSelectionPage.xaml.cs
@@ -32,37 +32,44 @@ namespace Celeste_Launcher_Gui.Pages
 
         private void SelectGerman(object sender, RoutedEventArgs e)
         {
-
+            SetLanguage(GameLanguage.deDE);
         }
 
         private void SelectEnglish(object sender, RoutedEventArgs e)
         {
-
+            SetLanguage(GameLanguage.enUS);
         }
 
         private void SelectSpanish(object sender, RoutedEventArgs e)
         {
-
+            SetLanguage(GameLanguage.esES);
         }
 
         private void SelectFrench(object sender, RoutedEventArgs e)
         {
-
+            SetLanguage(GameLanguage.frFR);
         }
 
         private void SelectItalian(object sender, RoutedEventArgs e)
         {
-
+            SetLanguage(GameLanguage.itIT);
         }
 
         private void SelectChinese(object sender, RoutedEventArgs e)
         {
-
+            SetLanguage(GameLanguage.zhCHT);
         }
 
         private void SelectPortugese(object sender, RoutedEventArgs e)
         {
+            //SetLanguage(GameLanguage.ptBR);
+        }
 
+        private void SetLanguage(GameLanguage language)
+        {
+            LegacyBootstrapper.UserConfig.GameLanguage = language;
+            LegacyBootstrapper.UserConfig.Save(LegacyBootstrapper.UserConfigFilePath);
+            NavigationService.GoBack();
         }
     }
 }
