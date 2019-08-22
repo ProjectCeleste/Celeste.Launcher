2ce51ade893efdc88a41092ab83c8ecbd7904525
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2019-08-22 21:45:21 +0200

    Implemented settings context menu, hook up rest of options on main page

--- Celeste_Launcher_Gui/Pages/OverviewPage.xaml.cs
+++ Celeste_Launcher_Gui/Pages/OverviewPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using Celeste_Launcher_Gui.Account;
+using Celeste_Launcher_Gui.Forms;
+using Celeste_Launcher_Gui.Helpers;
 using Celeste_Launcher_Gui.Services;
+using Celeste_Launcher_Gui.UserControls;
 using Celeste_Launcher_Gui.Windows;
 using Celeste_Public_Api.WebSocket_Api.WebSocket.CommandInfo.Member;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +16,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -60,12 +65,18 @@ namespace Celeste_Launcher_Gui.Pages
 
         private void OnAccountClick(object sender, RoutedEventArgs e)
         {
-            // TODO
+            using (var form = new ChangePwdForm())
+            {
+                form.ShowDialog();
+            }
         }
 
         private void OnFriendsClick(object sender, RoutedEventArgs e)
         {
-            // TODO
+            using (var x = new FriendsForm())
+            {
+                x.ShowDialog();
+            }
         }
 
         private void OnDonateClick(object sender, RoutedEventArgs e)
@@ -98,7 +109,8 @@ namespace Celeste_Launcher_Gui.Pages
 
         private void OnSettingsClick(object sender, RoutedEventArgs e)
         {
-            // TODO
+            SettingsButton.ContextMenu.PlacementTarget = sender as UIElement;
+            SettingsButton.ContextMenu.IsOpen = true;
         }
 
         private void OnAoeodbClick(object sender, RoutedEventArgs e)
@@ -117,6 +129,7 @@ namespace Celeste_Launcher_Gui.Pages
         }
         #endregion
 
+        #region Settings context menu callbacks
         private void OnLoginClick(object sender, RoutedEventArgs e)
         {
             LoginBtn.IsEnabled = false;
@@ -130,5 +143,104 @@ namespace Celeste_Launcher_Gui.Pages
             GameService.StartGame();
             PlayOfflineBtn.IsEnabled = true;
         }
+
+        private void OpenGameLanguage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Pages/LanguageSelectionPage.xaml", UriKind.Relative));
+        }
+
+        private void EnableDiagnosticsMode(object sender, RoutedEventArgs e)
+        {
+            LegacyBootstrapper.UserConfig.IsDiagnosticMode = !LegacyBootstrapper.UserConfig.IsDiagnosticMode;
+
+            if (LegacyBootstrapper.UserConfig.IsDiagnosticMode)
+            {
+                try
+                {
+                    var procdumpFileName = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "procdump.exe");
+                    if (!File.Exists(procdumpFileName))
+                    {
+                        var dialog = new GenericMessageDialog("ProcDump.exe need to be installed first. Would you like to install it now?" , DialogIcon.Warning, DialogOptions.YesNo);
+
+                        var dr = dialog.ShowDialog();
+                        if (dr.Value == true)
+                            using (var form2 = new InstallProcDump())
+                            {
+                                form2.ShowDialog();
+                            }
+                        else
+                            LegacyBootstrapper.UserConfig.IsDiagnosticMode = false;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    LegacyBootstrapper.UserConfig.IsDiagnosticMode = false;
+                    GenericMessageDialog.Show($"Warning: Failed to enable \"Diagnostic Mode\". Error message: {exception.Message}", DialogIcon.Warning, DialogOptions.Ok);
+                }
+            }
+        }
+
+        private void OpenSteam(object sender, RoutedEventArgs e)
+        {
+            using (var form = new SteamForm())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void OpenWindowsFeatures(object sender, RoutedEventArgs e)
+        {
+            var osInfo = OsVersionInfo.GetOsVersionInfo();
+            if (osInfo.Major < 6 || osInfo.Major == 6 && osInfo.Minor < 2)
+            {
+                GenericMessageDialog.Show("Only for Windows 8 and more\r\n" +
+                    $"Your current OS is {osInfo.FullName}",
+                    DialogIcon.Warning);
+
+                return;
+            }
+
+            using (var form = new WindowsFeaturesForm())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void OpenWindowsFirewall(object sender, RoutedEventArgs e)
+        {
+            using (var form = new FirewallForm())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void OpenGameScanner(object sender, RoutedEventArgs e)
+        {
+            var pname = Process.GetProcessesByName("spartan");
+            if (pname.Length > 0)
+            {
+                GenericMessageDialog.Show($@"Game is still running, please close it and try again", DialogIcon.Error, DialogOptions.Ok);
+                return;
+            }
+
+            using (var form = new GameScan())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void OpenUpdater(object sender, RoutedEventArgs e)
+        {
+            var pname = Process.GetProcessesByName("spartan");
+            if (pname.Length > 0)
+            {
+                GenericMessageDialog.Show($@"Game is still running, please close it and try again", DialogIcon.Error, DialogOptions.Ok);
+                return;
+            }
+
+            var updater = new UpdateWindow();
+            updater.ShowDialog();
+        }
+        #endregion
     }
 }
