2ef4a262d290327fcfce2474e79e8a1f125641d8
Ryddag <27833747+Ryddag@users.noreply.github.com>
2019-12-31 00:56:21 +0100

    Update UpdateService

--- Celeste_Launcher_Gui/Services/UpdateService.cs
+++ Celeste_Launcher_Gui/Services/UpdateService.cs
@@ -1,9 +1,11 @@
-﻿using Celeste_Launcher_Gui.Helpers;
+﻿#define IS_BETA
+
+using Celeste_Launcher_Gui.Helpers;
 using Celeste_Launcher_Gui.ViewModels;
 using Celeste_Public_Api.Helpers;
 using Markdig;
+using ProjectCeleste.GameFiles.GameScanner.FileDownloader;
 using System;
-using System.ComponentModel;
 using System.IO;
 using System.Net.Http;
 using System.Reflection;
@@ -14,15 +16,28 @@ using System.Web;
 
 namespace Celeste_Launcher_Gui.Services
 {
-    public class UpdateService
+    public static class UpdateService
     {
-        private const string AssemblyInfoUrl = "https://raw.githubusercontent.com/ProjectCeleste/Celeste_Launcher/master/Celeste_Launcher_Gui/Properties/AssemblyInfo.cs";
+#if IS_BETA
+        private const string AssemblyInfoUrl =
+                "https://raw.githubusercontent.com/ProjectCeleste/Celeste_Launcher/beta/Celeste_Launcher_Gui/Properties/AssemblyInfo.cs";
 
-        private const string ChangelogUrl = "https://raw.githubusercontent.com/ProjectCeleste/Celeste_Launcher/master/CHANGELOG.md";
+        private const string ChangelogUrl =
+            "https://raw.githubusercontent.com/ProjectCeleste/Celeste_Launcher/beta/CHANGELOG.md";
+#else
 
-        private const string ReleaseZipUrl = "https://github.com/ProjectCeleste/Celeste_Launcher/releases/download/v";
+        private const string AssemblyInfoUrl =
+                "https://raw.githubusercontent.com/ProjectCeleste/Celeste_Launcher/master/Celeste_Launcher_Gui/Properties/AssemblyInfo.cs";
 
-        public static async Task<Version> GetGitHubVersion()
+        private const string ChangelogUrl =
+            "https://raw.githubusercontent.com/ProjectCeleste/Celeste_Launcher/master/CHANGELOG.md";
+
+#endif
+
+        private const string ReleaseZipUrl =
+            "https://github.com/ProjectCeleste/Celeste_Launcher/releases/download/v";
+
+        public static async Task<Version> GetGitHubAssemblyVersion()
         {
             string version;
 
@@ -38,9 +53,7 @@ namespace Celeste_Launcher_Gui.Services
             if (!match.Success)
                 throw new Exception("GetLatestVersion() match.Success != true");
 
-            var gitVersion = new Version($"{match.Groups[1]}.{match.Groups[2]}.{match.Groups[3]}.{match.Groups[4]}");
-
-            return gitVersion;
+            return new Version($"{match.Groups[1]}.{match.Groups[2]}.{match.Groups[3]}.{match.Groups[4]}");
         }
 
         public static async Task<string> GetChangeLog()
@@ -75,7 +88,7 @@ namespace Celeste_Launcher_Gui.Services
         public static async Task LoadUpdateInfo(LauncherVersionInfo launcherVersionInfo)
         {
             launcherVersionInfo.CurrentVersion = Assembly.GetEntryAssembly().GetName().Version.ToString();
-            launcherVersionInfo.NewVersion = (await GetGitHubVersion()).ToString();
+            launcherVersionInfo.NewVersion = (await GetGitHubAssemblyVersion()).ToString();
             launcherVersionInfo.ChangeLog = await GetChangeLog();
         }
 
@@ -85,5 +98,109 @@ namespace Celeste_Launcher_Gui.Services
             var stripped = reg.Replace(htmlText, "");
             return decode ? HttpUtility.HtmlDecode(stripped) : stripped;
         }
+        public static async Task DownloadAndInstallUpdate(bool isSteam = false, IProgress<int> progress = null,
+            CancellationToken ct = default(CancellationToken))
+        {
+            Misc.CleanUpFiles(Directory.GetCurrentDirectory(), "*.old");
+
+            var gitVersion = await GetGitHubAssemblyVersion().ConfigureAwait(false);
+
+            progress?.Report(3);
+
+            if (gitVersion <= Assembly.GetExecutingAssembly().GetName().Version)
+                return;
+
+            ct.ThrowIfCancellationRequested();
+
+            const string zipName = "Celeste_Launcher.zip";
+#if IS_BETA
+            var downloadLink = $"{ReleaseZipUrl}{gitVersion.Major}.{gitVersion.Minor}.{gitVersion.Build}-beta/{zipName}";
+#else
+            var downloadLink = $"{ReleaseZipUrl}{gitVersion.Major}.{gitVersion.Minor}.{gitVersion.Build}/{zipName}";
+#endif
+
+            //Download File
+            progress?.Report(5);
+
+            var tempFileName = Path.GetTempFileName();
+
+            try
+            {
+                var downloadFileAsync = new SimpleFileDownloader(downloadLink, tempFileName);
+                if (progress != null)
+                    downloadFileAsync.ProgressChanged += (sender, args) =>
+                    {
+                        progress.Report(Convert.ToInt32(Math.Floor(70 * (downloadFileAsync.DownloadProgress / 100))));
+                    };
+                await downloadFileAsync.DownloadAsync(ct);
+            }
+            catch (AggregateException)
+            {
+                if (File.Exists(tempFileName))
+                    File.Delete(tempFileName);
+
+                throw;
+            }
+
+            //Extract File
+            progress?.Report(65);
+
+            Progress<double> extractProgress = null;
+            if (progress != null)
+            {
+                extractProgress = new Progress<double>();
+                extractProgress.ProgressChanged += (o, ea) =>
+                {
+                    progress.Report(70 + Convert.ToInt32(Math.Floor(20 * (ea / 100))));
+                };
+            }
+            var tempDir = Path.Combine(Path.GetTempPath(), $"Celeste_Launcher_v{gitVersion}");
+
+            if (Directory.Exists(tempDir))
+                Misc.CleanUpFiles(tempDir, "*.*");
+
+            try
+            {
+                await ZipUtils.ExtractZipFile(tempFileName, tempDir, extractProgress, ct);
+            }
+            catch (AggregateException)
+            {
+                Misc.CleanUpFiles(tempDir, "*.*");
+                throw;
+            }
+            finally
+            {
+                if (File.Exists(tempFileName))
+                    File.Delete(tempFileName);
+            }
+
+            //Move File
+            progress?.Report(90);
+
+            var destinationDir = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar;
+            try
+            {
+                Misc.MoveFiles(tempDir, destinationDir);
+            }
+            finally
+            {
+                Misc.CleanUpFiles(tempDir, "*.*");
+            }
+
+            //isSteam Version
+            if (isSteam)
+            {
+                progress?.Report(95);
+                Steam.ConvertToSteam(destinationDir);
+            }
+
+            //Clean Old File
+            progress?.Report(97);
+            Misc.CleanUpFiles(Directory.GetCurrentDirectory(), "*.old");
+
+            //
+            progress?.Report(100);
+        }
+
     }
 }
