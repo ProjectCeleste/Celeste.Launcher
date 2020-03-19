537fcd749d7cbb3ffb6e1dca6acffff30f8852e3
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2020-03-19 20:02:23 +0100

    Players should be able to import aoeo to Steam

--- Celeste_Launcher_Gui/Windows/SteamConverterWindow.xaml.cs
+++ Celeste_Launcher_Gui/Windows/SteamConverterWindow.xaml.cs
@@ -46,13 +46,6 @@ namespace Celeste_Launcher_Gui.Windows
 
                 var currentWorkingDirectory = Path.GetDirectoryName(currentApplicationFullPath);
 
-                if (!File.Exists($"{currentWorkingDirectory}\\steam_appid.txt") && !File.Exists($"{currentWorkingDirectory}\\steam_api.dll"))
-                {
-                    GenericMessageDialog.Show(Properties.Resources.SteamConverterIncorrectInstallationDirectory, DialogIcon.None, DialogOptions.Ok);
-                    Close();
-                    return;
-                }
-
                 LegacyBootstrapper.UserConfig.GameFilesPath = currentWorkingDirectory;
                 LegacyBootstrapper.UserConfig.Save(LegacyBootstrapper.UserConfigFilePath);
 
