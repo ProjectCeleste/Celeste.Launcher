dac16cd2ff1013e53469c270a3c0eb8828ee6c64
Kevin Waldock <kevin.waldock@gmail.com>
2024-05-09 16:01:10 +0200

    Allow settings from LegacyBootstrapper to be overriden by commandline

--- CelesteGameScannerUI/App.xaml.cs
+++ CelesteGameScannerUI/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Celeste_Launcher_Gui;
 using Celeste_Public_Api.Logging;
+using Serilog;
 using System;
 using System.Windows;
 
@@ -10,6 +11,8 @@ namespace CelesteGameScannerUI
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILogger Logger = LoggerFactory.GetLogger();
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             AppDomain.CurrentDomain.UnhandledException += (o, exArgs) =>
@@ -20,6 +23,24 @@ namespace CelesteGameScannerUI
             };
 
             LegacyBootstrapper.LoadUserConfig();
+
+            if (e.Args.Length >= 3)
+            {
+                try
+                {
+                    LegacyBootstrapper.UserConfig.GameFilesPath = e.Args[0];
+                    if (Enum.TryParse(e.Args[1], true, out GameLanguage gamelang))
+                    {
+                        LegacyBootstrapper.UserConfig.GameLanguage = gamelang;
+                    }
+                    LegacyBootstrapper.UserConfig.IsSteamVersion = bool.Parse(e.Args[2]);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, ex.Message);
+                }
+            }
+
             LegacyBootstrapper.SetUILanguage();
         }
     }
